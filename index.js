236f0cc8dbe34b9bb298c3a17bb01b4180f34a47
jaypee15 <okoyejohnpaul15@gmail.com>
2024-06-28 20:37:05 +0100

    feat: add user endpoints

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // load dependencies
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const ErrorHandler = require("./middlewares/error-handler");
+const userRoutes = require("./routes/user");
+
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
+
+//Routes
+app.use("/api/users", userRoutes);
 
 app.use("*", (req, res, next) => {
   console.log(`route ${req.baseUrl} not found`);
@@ -11,5 +20,7 @@ app.use("*", (req, res, next) => {
   res.status(404).json({ message: "not found" });
 });
 
+// Error Handler
+app.use(ErrorHandler)
 
 module.exports = app;
