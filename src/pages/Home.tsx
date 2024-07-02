804b8dd4830093070c08ca99c6f732f7d0adacb6
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-02 18:17:08 +0100

    Implement about component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "../components/HeroSection.tsx";
 import Products from "../components/products/Products.tsx";
+import AboutComponent from "../components/AboutComponent.tsx";
 
 const Home = () => {
     return (
         <div>
             <HeroSection/>
             <Products/>
+            <AboutComponent/>
         </div>
     );
 };
