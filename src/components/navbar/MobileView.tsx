6675cdf0982591eb268c6b3acfbd6d194de8f695
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-02 11:35:01 +0100

    Implement navbar UI

--- src/components/navbar/MobileView.tsx
+++ src/components/navbar/MobileView.tsx
@@ -2,17 +2,19 @@ import {GiHamburgerMenu} from "react-icons/gi";
 import {Sheet, SheetContent, SheetTrigger} from "../ui/sheet.tsx";
 import {Separator} from "../ui/separator.tsx";
 import NavItems from "./NavItems.tsx";
+import {QueryItems} from "./routeContants.tsx";
 
 const MobileView = () => {
     return (
-        <div className="md:hidden">
+        <div className="lg:hidden">
             <Sheet>
                 <SheetTrigger className="align-middle">
                     <GiHamburgerMenu size={25}/>
                 </SheetTrigger>
-                <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+                <SheetContent className="flex flex-col gap-6 bg-white lg:hidden">
                     <Separator className="border border-gray-50"/>
                     <NavItems/>
+                    <QueryItems/>
                 </SheetContent>
             </Sheet>
         </div>
